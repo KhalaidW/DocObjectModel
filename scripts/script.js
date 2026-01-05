fcc326f8ebf5ee8699bc52dea5e86bb9b3890166
KhalaidW <walidkhalaid@gmail.com>
2026-01-04 22:03:09 -0500

    second grid has hit the page

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,28 @@
-const grid = document.getElementById("fireField");
+const grid1 = document.getElementById("fireField");
 const grid2 = document.getElementById("hitField")
-const rows = 10;
-const cols = 10;
+// const rows = 10;
+// const cols = 10;
 
-for (let i = 0; i < rows * cols; i++) {
-  const cell = document.createElement("div");
-  cell.classList.add("cell");
+// for (let i = 0; i < rows * cols; i++) {
+//   const cell = document.createElement("div");
+//   cell.classList.add("cell");
 
-  // optional: click interaction
-  cell.addEventListener("click", () => {
-    cell.style.backgroundColor = "orange";
-  });
+//   // optional: click interaction
+//   cell.addEventListener("click", () => {
+//     cell.style.backgroundColor = "orange";
+//   });
 
-  grid.appendChild(cell);
-  grid2.appendChild(cell);
+//   grid.appendChild(cell);
+//   grid2.appendChild(cell);
+// }
+
+function createGrid(gridElement) {
+  for (let i = 0; i < 100; i++) {
+    const cell = document.createElement("div")
+    cell.classList.add("cell")
+    gridElement.appendChild(cell)
+  }
 }
+
+createGrid(grid1)
+createGrid(grid2)
